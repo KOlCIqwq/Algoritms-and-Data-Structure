d563bdcd0fe060887706d4127a26e668af0d9e92
kolci <kolci@duck.com>
2024-10-31 11:15:45 +0000

    Try maxInterval

--- 7IntervalSum.py
+++ 7IntervalSum.py
@@ -1,10 +1,3 @@
-#a = input()
-#b = input()
-a = [5, 0, 3, 1, 13, 7, 5]
-b = [0, 6, 2, 4, 3, 6]
-#arr = list(map(int,a.split(' ')))
-#intervals = list(map(int,a.split(' ')))
-
 def easySum(arr,intervals) -> int:
         n = len(arr)
         out = []
@@ -19,7 +12,6 @@ def easySum(arr,intervals) -> int:
                           sum += arr[j]
                 out.append(sum)
         return out
-print(easySum(a,b))
 
 def midSum(arr,intervals) -> int:
         n = len(arr)
@@ -35,5 +27,59 @@ def midSum(arr,intervals) -> int:
                 if left > right or left > n or right > n:
                           return 
                 out.append(contArray[right] - contArray[left])     
-        return out           
+        return out
+# Max-interval
+def buildSegmentTree(arr):
+    n = len(arr)
+    # Height of segment tree
+    height = (n - 1).bit_length()
+    # Maximum size of segment tree
+    max_size = 2 * (2 ** height) - 1
+    tree = [0] * max_size
+
+    def build(node, start, end):
+        if start == end:
+            # Leaf node will have a single element
+            tree[node] = arr[start]
+        else:
+            mid = (start + end) // 2
+            build(2 * node + 1, start, mid)
+            build(2 * node + 2, mid + 1, end)
+            tree[node] = max(tree[2 * node + 1], tree[2 * node + 2])
+
+    build(0, 0, n - 1)
+    return tree
+
+def querySegmentTree(tree, n, L, R):
+    def query(node, start, end, L, R):
+        if R < start or end < L:
+            return float('-inf')  # Out of range
+        if L <= start and end <= R:
+            return tree[node]  # Current segment is completely within range
+        mid = (start + end) // 2
+        left_max = query(2 * node + 1, start, mid, L, R)
+        right_max = query(2 * node + 2, mid + 1, end, L, R)
+        return max(left_max, right_max)
+
+    return query(0, 0, n - 1, L, R)
+
+def maxInterval(arr, intervals):
+    n = len(arr)
+    tree = buildSegmentTree(arr)
+
+    results = []
+    for l in range(0,len(intervals),2):
+        r = l + 1
+        results.append(querySegmentTree(tree, n, l, r))
+    
+    return results
+
+#a = input()
+#b = input()
+#arr = list(map(int,a.split()))
+#intervals = list(map(int,b.split()))
+a = [5, 0, 3, 1, 13, 7, 5]
+b = [0, 6, 2, 4, 3, 6]
+print(easySum(a,b))           
 print(midSum(a,b))
+print(maxInterval(a,b))
