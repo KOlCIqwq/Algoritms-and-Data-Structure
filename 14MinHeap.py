d8c07d8892f47a7f9b29aa5230d93e0451d80969
kolci <kolci@duck.com>
2024-12-04 22:30:46 +0100

    Finished 14

--- 14MinHeap.py
+++ 14MinHeap.py
@@ -1,17 +1,145 @@
-class minHeap:
-    def __init__(self,top,heap):
-        self.heap = heap
-        self.top = top
-    def build(self,values):
-        values.sort()
-        self.heap = values
-        self.top = values[0]
-    def length(self):
-        return len(self.heap)
-    def getmin(self):
-        return self.top
-    def extract(self,x):
+class MinHeap:
+    def __init__(self):
+        self.heap = []
 
-    def find(self,x):
+    # Build an heap from an array
+    # @param: arr, an array not null, if null return None
+    # @return: None
+    # Time: O(n), n as length of arr
+    def build(self, arr):
+        self.heap = []
+        self.heap = arr
+        n = len(arr)
+        if n == 0:
+            print("Can't be null")
+            return
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapifyDown(i)
+    
+    # Get the length of the heap
+    # @param: None
+    # @return: an integer
+    # Time: O(1)
+    def length(self):
+        if self.heap == None:
+            return 0
+        return len(self.heap)
+
+    # Peek the root
+    # @param: None
+    # @return: first element, but doesn't remove it from heap
+    # Time: O(1)
+    def getmin(self):
+        if not self.heap:
+            return None
+        return self.heap[0]
+
+    # Pop the root
+    # @param: None
+    # @return: first element and remove it from heap
+    # Time: O(n), for heapify
+    def extract(self):
+        if not self.heap:
+            return None
+        minVal = self.heap[0]
+        if len(self.heap) > 1:
+            self.heap[0] = self.heap.pop()  # Replace root with the last element
+            self.heapifyDown(0)
+        else:
+            self.heap.pop()  # Remove the last element directly
+        return minVal
+
+    # Insert an element to the heap
+    # @param: x
+    # @return: None
+    # Time: O(n), for heapify
+    def insert(self, x):
+        self.heap.append(x)
+        self.heapifyUp(len(self.heap) - 1)
+
+    # Change the value at i to x, heapify up and down
+    # @param: i, x
+    # @return: None
+    # Time: O(n)
+    def change(self, i, x):
+        if i < 0 or i >= len(self.heap):
+            print(f"Invalid index: {i}")
+            return
+        self.heap[i] = x
+        self.heapifyUp(i)
+        self.heapifyDown(i)
+    
+    # From a given index, heapify up 
+    # @param: i
+    # @return: None
+    # Time: O(n)
+    def heapifyUp(self, i):
+        while i > 0:
+            parent = (i - 1) // 2
+            if self.heap[parent] > self.heap[i]:
+                self.heap[parent], self.heap[i] = self.heap[i], self.heap[parent]
+                i = parent
+            else:
+                break
+
+    # Given an index, heapify down
+    # @param: i
+    # @return: None
+    # Time: O(n)
+    def heapifyDown(self, i):
+        arr = self.heap
+        n = len(arr)
+        small = i
+        left = i * 2 + 1
+        right = left + 1
+
+        if left < n and arr[left] < arr[small]:
+            small = left
+        if right < n and arr[right] < arr[small]:
+            small = right
+
+        if small != i:
+            arr[i], arr[small] = arr[small], arr[i]
+            self.heapifyDown(small)
+    def getHeap(self):
+        return self.heap
+
+def processCommands(commands):
+    heap = MinHeap()
+    
+    while True:
+        command = input().strip()
+        parts = command.split()
+        operation = parts[0]
+
+        if operation == "build":
+            arr = list(map(int, parts[1:]))
+            heap.build(arr)
+            print(*heap.getHeap())
         
-    def heapify(self,x):
+        elif operation == "length":
+            print(heap.length())
+        
+        elif operation == "getmin":
+            print(heap.getmin())
+        
+        elif operation == "insert":
+            x = int(parts[1])
+            heap.insert(x)
+            print(*heap.getHeap())
+        
+        elif operation == "extract":
+            print(heap.extract())
+            print(*heap.getHeap())
+        
+        elif operation == "change":
+            i = int(parts[1])
+            x = int(parts[2])
+            heap.change(i, x)
+            print(*heap.getHeap())
+        
+        elif operation == "exit":
+            break
+
+a = input()
+processCommands(a)
