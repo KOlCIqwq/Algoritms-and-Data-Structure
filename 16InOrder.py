86181bc1ecb5b294137dffa1a8af9a5672b04ee9
kolci <kolci@duck.com>
2024-12-11 21:39:13 +0100

    Added the procedure

--- 16InOrder.py
+++ 16InOrder.py
@@ -1,4 +1,12 @@
 def InOrder(arr,out):
+    '''
+    1. Since the input is in polish form, the input tree will be from left to right
+    2. Each time we pop the first element of the input array, if this element is null return None
+    3. We traverse to the left by recalling the function (given the nature of the polish form)
+       if it arrived at the end it will append for first the rightmost node 
+    4. It will traverse to the right when we can't move the left anymore, appending the leftmost right node
+    O(n) we need traverse all the nodes anyway
+    '''
     if not arr: # Parsed the whole tree
         return 
     current = arr.pop(0) # Current node
